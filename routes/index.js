399553bffd504e7fd848ae9679afca4c2e5ff178
Muhammad Rohman Irfanuddin <muhammad.rohman.irfanuddin09@gmail.com>
2018-12-27 12:16:39 +0700

    edit & delete data

--- routes/index.js
+++ routes/index.js
@@ -21,4 +21,33 @@ router.post('/add', function(req, res, next) {
   res.render('add', { title: 'Add Data Mahasiswa'});
 });
 
+/* GET-POST edit data */
+router.get('/edit/:id', function(req, res, next) {
+  var id = req.params.id;
+  connection.query(`SELECT * FROM mahasiswa WHERE id=${id}`, function (error, results, fields) {
+    if (error) throw error;
+    console.log( results[0] );
+    res.render('edit', { title: 'Edit Data Mahasiswa', data: results[0] });
+  });
+});
+router.post('/edit/:id', function(req, res, next) {
+  var id      = req.params.id;
+  var nama    = req.body.nama;
+  var nim     = req.body.nim;
+  var jurusan = req.body.jurusan;
+  connection.query(`UPDATE mahasiswa SET nama='${nama}', nim='${nim}', jurusan='${jurusan}' WHERE id=${id}`, function (error, results, fields) {
+    if (error) throw error;
+    res.redirect('/');
+  });
+});
+
+/* GET delete data. */
+router.get('/del/:id', function(req, res, next) {
+  var id      = req.params.id;
+  connection.query(`DELETE FROM mahasiswa WHERE id=${id}`, function (error, results, fields) {
+    if (error) throw error;
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
