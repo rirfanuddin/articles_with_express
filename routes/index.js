572a5e568178d46f0c22f2f236b2477c9675328d
Muhammad Rohman Irfanuddin <muhammad.rohman.irfanuddin09@gmail.com>
2018-12-27 12:08:06 +0700

    add a new data

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page - list all data mahasiswa */
 router.get('/', function(req, res, next){
   connection.query('SELECT * FROM mahasiswa', function(error, results, fields){
     if(error) throw error;
     res.render('index', {title : 'Get all data mahasiswa', data : results})
   });
+});
+
+/* POST add data mahasiswa */
+router.get('/add', function(req, res, next) {
+  res.render('add', { title: 'Add Data Mahasiswa' });
+});
+router.post('/add', function(req, res, next) {
+  connection.query(`INSERT INTO mahasiswa(nama,nim,jurusan) VALUES('${req.body.nama}','${req.body.nim}','${req.body.jurusan}')`, function (error, results, fields) {
+    if (error) throw error;
+    console.log('Sukses');
+  });
+  res.render('add', { title: 'Add Data Mahasiswa'});
 });
 
 module.exports = router;
